8bbd5f5c3a4ba0eaf64511b693664153dde332a1
monseeker <guesam460@gmail.com>
2025-05-20 13:12:55 -0300

    Ejercicio 1

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Ingrese un numero: ");
+string a = Console.ReadLine();
+
+float num;
+if (float.TryParse(a, out num))
+{
+    if (num > 0)
+    {
+        num = 1/num;
+    }
+    Console.WriteLine(num);
+}
