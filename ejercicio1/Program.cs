d45ae5ab4a002b69b09f4b522bd2cc89718e1d22
monseeker <guesam460@gmail.com>
2025-05-28 16:48:38 -0300

    Ejercicio 1

---
+++ ejercicio1/Program.cs
@@ -0,0 +1,18 @@
+﻿Console.Write("Ingrese un número: ");
+string? cadena = Console.ReadLine();
+int num;
+if (int.TryParse(cadena, out num))
+{
+    int aux = 0;
+    int longitud = cadena.Length;
+    if (num > 0)
+    {
+        while (longitud > 0)
+        {
+            aux += (num % 10) * (int)(Math.Pow(10, longitud - 1));
+            num /= 10;
+            longitud--;
+        }
+    }
+    Console.WriteLine("Inverso: " + aux);
+}
