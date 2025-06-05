c57646b575c08f0a2d41af1b5c6c8b1292b97ed4
monseeker <guesam460@gmail.com>
2025-06-05 02:17:49 -0300

    Calculadora V2 corregida

--- ejercicio2/Program.cs
+++ ejercicio2/Program.cs
@@ -1,136 +1,59 @@
 ﻿// See https://aka.ms/new-console-template for more information
-using System;
 
-int realizarOtraOperacion = 1;
 Console.WriteLine("Calculadora Avanzada");
+double primerNumeroACalcular = PedirNumero();
+Console.WriteLine("\n");
 
-do{
-    Console.WriteLine("1. Valor Absoluto");
-    Console.WriteLine("2. Cuadrado");
-    Console.WriteLine("3. Raiz Cuadrada");
-    Console.WriteLine("4. Seno");
-    Console.WriteLine("5. Coseno");
-    Console.WriteLine("6. Parte Entera de un Real");
-    Console.WriteLine("7. Max y Min entre dos Numeros");
-    
-    Console.Write("\nIngrese la operacion: ");
+double resultadoAbsoluto = Math.Abs(primerNumeroACalcular);
+Console.WriteLine($"Valor Absoluto de {primerNumeroACalcular}: {resultadoAbsoluto} \n");
 
-    string cadena = Console.ReadLine();
-    
-    if (int.TryParse(cadena, out int operacion))
-    {
-        switch (operacion)
-        {
-            case 1:
-                Console.WriteLine("\nValor Absoluto");
-                double resultadoAbsoluto = Math.Abs(Numeros());
-                Console.WriteLine($"Resultado: {resultadoAbsoluto} \n");
-                break;
-            case 2:
-                Console.WriteLine("\nCuadrado");
-                double resultadoPotencia = Math.Pow(Numeros(), 2);
-                Console.WriteLine($"Resultado: {resultadoPotencia} \n");
-                break;
-            case 3:
-                Console.WriteLine("\nRaiz Cuadrada");
-                double numero = Numeros();
-                if (numero >= 0)
-                {
-                    double resultadoRaiz = Math.Sqrt(numero);
-                    Console.WriteLine($"Resultado: {resultadoRaiz} \n");
-                }
-                else
-                {
-                    Console.WriteLine("Ingrese un numero no negativo. \n");
-                }
-                break;
-            case 4:
-                Console.WriteLine("\nSeno");
-                double anguloGradosSeno = Numeros();
-                double resultadoSeno = Math.Sin(anguloGradosSeno * Math.PI / 180);
-                Console.WriteLine($"Seno de {anguloGradosSeno} grados: {resultadoSeno} \n");
-                break;
-            case 5:
-                Console.WriteLine("\nCoseno");
-                double anguloGradosCoseno = Numeros();
-                double resultadoCoseno = Math.Cos(anguloGradosCoseno * Math.PI / 180);
-                Console.WriteLine($"Coseno de {anguloGradosCoseno} grados: {resultadoCoseno} \n");
-                break;
-            case 6:
-                Console.WriteLine("\nEntero de un real");
-                int resultadoEntero = (int)Numeros();
-                Console.WriteLine($"Resultado: {resultadoEntero} \n");
-                break;
-            case 7:
-                Console.WriteLine("\nMayor y menor");
-                double primerNumero = Numeros();
-                double segundoNumero = Numeros();
-                if (primerNumero > segundoNumero)
-                {
-                    Console.WriteLine($"Entre {primerNumero} y {segundoNumero} el mayor es {primerNumero} y el menor {segundoNumero} \n");
-                }
-                else if (segundoNumero > primerNumero)
-                {
-                    Console.WriteLine($"Entre {primerNumero} y {segundoNumero} el mayor es {segundoNumero} y el menor {primerNumero} \n");
-                }
-                else
-                {
-                    Console.WriteLine("Ambos numeros son iguales. \n");
-                }
-                break;
-            default:
-                Console.WriteLine("Ingrese un valor valido. \n");
-                break;
-        }
+double resultadoPotencia = Math.Pow(primerNumeroACalcular, 2);
+Console.WriteLine($"Cuadrado de {primerNumeroACalcular}: {resultadoPotencia} \n");
 
-    }
-    else
-    {
-        Console.WriteLine("Ingrese un numero. \n");
-    }
+if (primerNumeroACalcular >= 0)
+{
+    double resultadoRaiz = Math.Sqrt(primerNumeroACalcular);
+    Console.WriteLine($"Raiz cuadrada de {primerNumeroACalcular}: {resultadoRaiz} \n");
+}
+else
+{
+    Console.WriteLine("No existe la raiz cuadrada de un numero negativo. \n");
+}
 
-    bool ingresoValorCorrecto = false;
+double resultadoSeno = Math.Sin(primerNumeroACalcular * Math.PI / 180);
+Console.WriteLine($"Seno de {primerNumeroACalcular} grados: {resultadoSeno} \n");
+
+double resultadoCoseno = Math.Cos(primerNumeroACalcular * Math.PI / 180);
+Console.WriteLine($"Coseno de {primerNumeroACalcular} grados: {resultadoCoseno} \n");
+
+double resultadoEntero = Math.Truncate(primerNumeroACalcular);
+Console.WriteLine($"Entero del real {primerNumeroACalcular}: {resultadoEntero} \n");
+
+
+Console.WriteLine("Ingrese dos numeros para comparar: ");
+double primerNumeroAComparar = PedirNumero();
+double segundoNumeroAComparar = PedirNumero();
+
+double numeroMaximo = Math.Max(primerNumeroAComparar, segundoNumeroAComparar);
+double numeroMinimo = Math.Min(primerNumeroAComparar, segundoNumeroAComparar);
+
+Console.WriteLine($"Maximo: {numeroMaximo}");
+Console.WriteLine($"Minimo: {numeroMinimo}");
+
+
+double PedirNumero()
+{
+    double num;
+    bool numeroValido;
     do
     {
-        Console.WriteLine("Desea realizar otra operacion?");
-        Console.WriteLine("1.Si \t0.No");
-        Console.Write("Desicion: ");
-        cadena = Console.ReadLine();
-        Console.WriteLine("\n");
-        if (int.TryParse(cadena, out realizarOtraOperacion))
+        Console.Write("Ingrese un numero: ");
+        string entrada = Console.ReadLine();
+        numeroValido = double.TryParse(entrada, out num);
+        if (!numeroValido)
         {
-            if (realizarOtraOperacion != 1 && realizarOtraOperacion != 0)
-            {
-                Console.WriteLine("Ingrese un valor valido. \n");
-            }
-            else
-            {
-                ingresoValorCorrecto = true;
-            }
+            Console.WriteLine("Ingrese un numero valido.");
         }
-        else
-        {
-            Console.WriteLine("Ingrese un numero. \n");
-
-        }
-    } while (ingresoValorCorrecto != true);
-
-} while (realizarOtraOperacion != 0);
-
-
-
-
-double Numeros()
-{
-    Console.Write("Ingrese un numero: ");
-    string entrada = Console.ReadLine();
-    if (double.TryParse(entrada, out double num))
-    {
-        return num;
-    }
-    else
-    {
-        Console.WriteLine("Ingrese un numero.");
-        return 0;
-    }
-}
+    } while (!numeroValido);
+    return num;
+} 
