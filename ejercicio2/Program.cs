ef12ec284b3e4072e0ce913d3d988be260161f7d
monseeker <guesam460@gmail.com>
2025-06-04 23:49:43 -0300

    Calculadora V2 con errores

--- ejercicio2/Program.cs
+++ ejercicio2/Program.cs
@@ -1,7 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
 
-int desicion = 1;
+int realizarOtraOperacion = 1;
 Console.WriteLine("Calculadora Avanzada");
 
 do{
@@ -11,36 +11,63 @@ do{
     Console.WriteLine("4. Seno");
     Console.WriteLine("5. Coseno");
     Console.WriteLine("6. Parte Entera de un Real");
-    Console.WriteLine("4. Max y Min entre dos Numeros");
+    Console.WriteLine("7. Max y Min entre dos Numeros");
     
     Console.Write("\nIngrese la operacion: ");
 
-    string? cadena = Console.ReadLine();
+    string cadena = Console.ReadLine();
     
     if (int.TryParse(cadena, out int operacion))
     {
-        int resultado;
-
         switch (operacion)
         {
             case 1:
                 Console.WriteLine("Valor Absoluto");
-                resultado = Math.Abs(Numeros());
-                Console.WriteLine("Resultado: " + resultado);
+                double resultadoAbsoluto = Math.Abs(Numeros());
+                Console.WriteLine("Resultado: " + resultadoAbsoluto);
                 break;
             case 2:
-                resultado = Math.Pow(Numeros(), 2);
-                Console.WriteLine("Resultado: " + resultado);
+                double resultadoPotencia = Math.Pow(Numeros(), 2);
+                Console.WriteLine("Resultado: " + resultadoPotencia);
                 break;
             case 3:
-                resultado = Math.Sqrt(Numeros());
-                Console.WriteLine("Resultado: " + resultado);
+                double = numero Numeros();
+                if (numero >= 0)
+                {
+                    double resultadoRaiz = Math.Sqrt(numero);
+                    Console.WriteLine("Resultado: " + resultadoRaiz);
+                } else {
+                    Console.WriteLine("Ingrese un numero no negativo.");
+                }
                 break;
             case 4:
-                resultado = ;
-                if (resultado != 0)
+                double anguloGradosSeno = Numeros();
+                double resultadoSeno = Math.Sin(anguloGradosSeno * Math.PI / 180);
+                Console.WriteLine($"Seno de {anguloGradosSeno} grados: {resultadoSeno}");
+                break;
+            case 5:
+                double anguloGradosCoseno = Numeros();
+                double resultadoCoseno = Math.Cos(anguloGradosCoseno * Math.PI / 180);
+                Console.WriteLine($"Coseno de {anguloGradosCoseno} grados: {resultadoCoseno}");
+                break;
+            case 6:
+                int resultadoEntero = (int)(Numeros());
+                Console.WriteLine("Resultado: " + resultadoEntero);
+                break;
+            case 7:
+                double primerNumero = Numeros();
+                double segundoNumero = Numeros();
+                if (primerNumero > segundoNumero)
                 {
-                    Console.WriteLine("Resultado: " + resultado);
+                    Console.WriteLine($"Entre {primerNumero} y {segundoNumero} el mayor es {primerNumero}");
+                }
+                else if (segundoNumero > primerNumero)
+                {
+                    Console.WriteLine($"Entre {primerNumero} y {segundoNumero} el mayor es {segundoNumero}");
+                }
+                else
+                {
+                    Console.WriteLine("Ambos numeros son iguales.");
                 }
                 break;
             default:
@@ -52,22 +79,24 @@ do{
     {
         Console.WriteLine("Ingrese un numero. \n");
     }
-    int aux = 0;
+
+    bool ingresoValorCorrecto = false;
     do
     {
         Console.WriteLine("Desea realizar otra operacion?");
         Console.WriteLine("1.Si \t0.No");
         Console.Write("Desicion: ");
         cadena = Console.ReadLine();
-        if (int.TryParse(cadena, out desicion))
+        if (int.TryParse(cadena, out realizarOtraOperacion))
         {
-            if (desicion != 1 && desicion != 0)
+            if (realizarOtraOperacion != 1 && realizarOtraOperacion != 0)
             {
                 Console.WriteLine("Metele algo valido. \n");
             }
             else
             {
-                aux = 3;
+                Console.WriteLine("\nIngrese la operacion:");
+                ingresoValorCorrecto = true;
             }
         }
         else
@@ -75,52 +104,24 @@ do{
             Console.WriteLine("Ingrese un numero. \n");
 
         }
-    } while (aux != 3);
+    } while (ingresoValorCorrecto != true);
 
-} while (desicion != 0);
-
-
+} while (realizarOtraOperacion != 0);
 
 
-int Numeros()
+
+
+double Numeros()
 {
     Console.Write("Ingrese un numero: ");
-    string? entrada = Console.ReadLine();
-    if (int.TryParse(entrada, out int num))
+    string entrada = Console.ReadLine();
+    if (double.TryParse(entrada, out double num))
     {
         return num;
     }
     else
     {
         Console.WriteLine("Ingrese un numero.");
-        return 0;
-    }
-}
-
-int Sumar(int num1, int num2)
-{
-    return num1 + num2;
-}
-
-int Restar(int num1, int num2)
-{
-    return num1 - num2;
-}
-
-int Producto(int num1, int num2)
-{
-    return num1 * num2;
-}
-
-int Cociente(int num1, int num2)
-{
-    if (num2 != 0)
-    {
-        return num1 / num2;
-    }
-    else
-    {
-        Console.WriteLine("No es posible dividir un numero en 0.");
         return 0;
     }
 }
