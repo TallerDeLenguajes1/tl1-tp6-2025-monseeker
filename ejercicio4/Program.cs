73f07090b5fcacf6d9e6bdf76e3e10dfd66f810f
monseeker <guesam460@gmail.com>
2025-06-09 02:10:50 -0300

    ejercicio 4 completo

--- ejercicio4/Program.cs
+++ ejercicio4/Program.cs
@@ -30,3 +30,78 @@ foreach (char c in CadenaConcatenada)
     Console.WriteLine(c);
 }
 
+Console.Write("Ingrese la palabra a buscar: ");
+string PalabraABuscar = Console.ReadLine();
+
+int contador = 0;
+int posicion = 0;
+
+while ((posicion = CadenaConcatenada.IndexOf(PalabraABuscar, posicion)) != -1)
+{
+    contador++;
+    posicion += PalabraABuscar.Length;
+}
+
+Console.WriteLine($"La palabra \"{PalabraABuscar}\" aparece {contador} veces.\n");
+
+Console.WriteLine($"\nEn mayuscula: {CadenaConcatenada.ToUpper()}\n");
+Console.WriteLine($"En minuscula: {CadenaConcatenada.ToLower()}\n");
+
+string[] CadenaSeparada = CadenaConcatenada.Split(' ');
+
+foreach (string cadenita in CadenaSeparada)
+{
+    Console.WriteLine(cadenita);
+}
+
+Console.Write("Ingrese una ecuacion simple: ");
+string ecuacion = Console.ReadLine();
+
+char[] operadores = { '+', '-', '*', '/' };
+char operador = ' ';
+
+foreach (char operacion in operadores)
+{
+    if (ecuacion.Contains(operacion))
+    {
+        operador = operacion;
+        break;
+    }
+}
+
+if (operador == ' ')
+{
+    Console.WriteLine("No se encontro un operador valido.\n");
+    return;
+}
+
+string[] numeros = ecuacion.Split(operador);
+
+if (numeros.Length != 2)
+{
+    Console.WriteLine("Ingrese una operacion simple.\n");
+    return;
+}
+
+if (int.TryParse(numeros[0], out int numero1) && int.TryParse(numeros[1], out int numero2))
+{
+    switch (operador)
+    {
+
+        case '+': Console.WriteLine($"{numero1}+{numero2}={numero1+numero2}"); break;
+        case '-': Console.WriteLine($"{numero1}-{numero2}={numero1-numero2}"); break;
+        case '*': Console.WriteLine($"{numero1}*{numero2}={numero1*numero2}"); break;
+        case '/':
+            if (numero2 != 0)
+            {
+                Console.WriteLine($"{numero1}/{numero2}={numero1/numero2}");
+            } else {
+                Console.WriteLine("No es posible dividir por 0.\n");
+                return;
+            }
+        break;
+    }
+} else {
+    Console.WriteLine("Ingrese numeros validos.\n");
+}
+
